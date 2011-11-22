c7e0471fc80a383c95dbf2e0e5bf81054446e265
Andy Brown <click.click.moon@gmail.com>
2011-11-22 08:50:01 -0800

    Updated to use express 3 style

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-require('express');
+var express = require('express'),
+	path = require('path'),
+
+	site = express();
+
+
+site.configure(function () {
+	site.use(express.favicon());
+	site.use(express.bodyParser());
+	site.use(express.static( path.join(__dirname, 'webui') ));
+	site.use(express.errorHandler({ dumpExceptions: true, showStack: true}));
+
+	site.listen(8675);
+});
